26480e8073b5794e4e7f4aed7cbc8e9c8fef1f3b
PrinseSasuke <sergey.pirkov@mail.ru>
2023-02-28 14:23:50 +0300

    new feautere in endpoints, get("/") for products returns list with dicts

--- app/endpoints/products.py
+++ app/endpoints/products.py
@@ -7,9 +7,15 @@ from app.templates_jinja import templates
 router = APIRouter()
 
 @router.get("/", response_model=List[Product])
-async def read_products(request: Request, products: ProductRepository = Depends(get_product_repository),
-                        limit: int = 100, skip: int = 0):
-    return products.get_all(limit=limit, skip=skip)
+async def read_products(products: ProductRepository = Depends(get_product_repository),
+                        limit: int = 100,
+                        skip: int = 0,):
+    products_list = await products.get_all(limit=limit, skip=skip)
+    rec_list = []
+    for rec in products_list:
+        rec_list.append(dict(rec))
+    print(rec_list)
+    return await products.get_all(limit=limit, skip=skip)
 
 @router.post("/", response_model=Product)
 async def create_job(
